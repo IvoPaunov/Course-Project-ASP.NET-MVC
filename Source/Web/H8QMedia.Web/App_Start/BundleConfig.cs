4312742823231eb18a282cd8ae52ba36b940e131
Ivo Paounov <ivo.paunov@gmail.com>
2016-02-12 13:38:37 +0200

    Custom css

--- Source/Web/H8QMedia.Web/App_Start/BundleConfig.cs
+++ Source/Web/H8QMedia.Web/App_Start/BundleConfig.cs
@@ -23,8 +23,10 @@
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-theme.css",
+                        "~/Content/custom.min.css",
+                        "~/Content/site.css"));
         }
     }
 }
