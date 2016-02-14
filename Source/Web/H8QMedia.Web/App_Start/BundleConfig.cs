698c2828d23f539c1690759534e75515e9f21ef2
Ivo Paounov <ivo.paunov@gmail.com>
2016-02-14 15:25:31 +0200

    Added Kendo UI wrappers; Added Kendo source files git ignore;

--- Source/Web/H8QMedia.Web/App_Start/BundleConfig.cs
+++ Source/Web/H8QMedia.Web/App_Start/BundleConfig.cs
@@ -8,10 +8,14 @@
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/Kendo/kendo.all.min.js",
+                "~/Scripts/Kendo/kendo.aspnetmvc.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -26,8 +30,12 @@
                         "~/Content/bootstrap.css",
                         "~/Content/bootstrap-theme.css",
                         "~/Content/custom.min.css",
-                        "~/Content/site.css",
-                        "~/Content/font-awesome.min.css"));
+                        "~/Content/font-awesome.min.css",
+                        "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo-css").Include(
+                  "~/Content/Kendo/kendo.common.min.css",
+                  "~/Content/Kendo/kendo.metro.min.css"));
         }
     }
 }
